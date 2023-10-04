a48cb8545b517fed76db2a34bd2222fbe73df5dc
Alok Maurya <47533834+alokmauryajnp@users.noreply.github.com>
2023-10-04 12:02:08 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+// function menufunction(){
+//     document.querySelector("ul").style.display ='block'
+//     document.querySelector(".ri-menu-line").style.display = 'none'
+// }
+
+
+
+function menufunction(){
+        document.querySelector("ul").style.display ='block'
+        document.querySelector(".ri-menu-line").style.display = 'none'
+        document.querySelector(".ri-close-line").style.display = 'block'
+}
+function closefunction(){
+    document.querySelector("ul").style.display ='none'
+    document.querySelector(".ri-menu-line").style.display = 'block'
+    document.querySelector(".ri-close-line").style.display = 'none'
+}
